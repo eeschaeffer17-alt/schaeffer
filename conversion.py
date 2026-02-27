8b71b0bea5dfa638f9a6027992b4f1b3c5e3dd2f
eeschaeffer17-alt <eeschaeffer17@gmail.com>
2026-02-27 14:52:38 -0500

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,10 @@
+def title():
+    print("Feet and Meters Converter")
+
+def fTm(feet):
+    meters=feet*0.3048
+    return meters
+
+def mTf(meters):
+    feet=meters/0.3048
+    return feet
---
+++ taxcalc.py
@@ -0,0 +1,9 @@
+def salesTax(total):
+    """calculates and returns the sales tax from the total purchase"""
+    tax=total*0.6
+    return tax
+def salesTaxTotal(total):
+    """calculates and returns the total cost of the purchase and sales tax"""
+    tax=total*0.6
+    after=total+tax
+    return after
